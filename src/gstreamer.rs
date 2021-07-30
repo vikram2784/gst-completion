5b5c8192f61337052dfd58c769d79263d8e81597
Vikram Fugro <vikram.fugro@gmail.com>
2021-07-30 22:54:33 +0530

    Initial Commit

---
+++ src/gstreamer.rs
@@ -0,0 +1,194 @@
+use gst;
+use gst::prelude::*;
+use gst::{Caps, Element, ElementFactory};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref LIST: Vec<ElementFactory> = {
+        gst::init().unwrap();
+        gst::ElementFactory::list_get_elements(0xffffffff, gst::Rank::__Unknown(0))
+    };
+}
+
+pub struct BashGstElement {
+    factory: ElementFactory,
+    element: Element,
+    caps: Caps,
+}
+
+impl BashGstElement {
+    pub fn get_property_names(&self, filter: &[&str], prefix: Option<&str>) -> Vec<String> {
+        self.element
+            .list_properties()
+            .into_iter()
+            .filter_map(|x| {
+                let name = x.get_name();
+
+                match if let Some(p) = prefix {
+                    if name.starts_with(p) {
+                        &name
+                    } else {
+                        ""
+                    }
+                } else {
+                    &name
+                } {
+                    "" => None,
+                    x if !filter.contains(&x) => Some(x.to_owned()),
+                    _ => None,
+                }
+            })
+            .collect()
+    }
+
+    pub fn get_compatible_elements(&self, prefix: Option<&str>) -> Vec<String> {
+        let mut v =
+            gst::ElementFactory::list_filter(&LIST, &self.caps, gst::PadDirection::Sink, false);
+
+        if let Some(p) = prefix {
+            v = v
+                .into_iter()
+                .filter(|x| x.get_name().starts_with(p))
+                .collect::<Vec<ElementFactory>>();
+        }
+
+        v.sort_by(|a, b| {
+            let anycaps = gst::Caps::new_any();
+
+            if a.can_sink_all_caps(&anycaps) {
+                return std::cmp::Ordering::Greater;
+            } else if b.can_sink_all_caps(&anycaps) {
+                return std::cmp::Ordering::Less;
+            }
+
+            let acaps = a
+                .get_static_pad_templates()
+                .iter()
+                .filter(|x| x.direction() == gst::PadDirection::Sink)
+                .fold(gst::Caps::new_empty(), |mut x, y| {
+                    x.merge(y.get_caps());
+                    x
+                });
+
+            let bcaps = b
+                .get_static_pad_templates()
+                .iter()
+                .filter(|x| x.direction() == gst::PadDirection::Sink)
+                .fold(gst::Caps::new_empty(), |mut x, y| {
+                    x.merge(y.get_caps());
+                    x
+                });
+
+            if acaps.is_subset(&bcaps) {
+                std::cmp::Ordering::Less
+            } else {
+                std::cmp::Ordering::Greater
+            }
+        });
+
+        v.into_iter().map(|x| x.get_name().to_string()).collect()
+    }
+}
+
+fn get_src_caps(factory: &ElementFactory, pad: Option<&str>) -> Caps {
+    factory
+        .get_static_pad_templates()
+        .into_iter()
+        .filter(|x| {
+            if x.direction() == gst::PadDirection::Src {
+                if let Some(p) = pad {
+                    x.name_template().starts_with(p)
+                } else {
+                    true
+                }
+            } else {
+                false
+            }
+        })
+        .fold(gst::Caps::new_empty(), |mut x, y| {
+            x.merge(y.get_caps());
+            x
+        })
+}
+
+pub fn get_elements(prefix: Option<&str>) -> Vec<String> {
+    LIST.iter()
+        .filter_map(|x| {
+            let name = x.get_name().to_string();
+
+            if let Some(p) = prefix {
+                if name.starts_with(p) {
+                    Some(name)
+                } else {
+                    None
+                }
+            } else {
+                Some(name)
+            }
+        })
+        .collect()
+}
+
+pub fn find_element(name: &str, pad: Option<&str>) -> Option<BashGstElement> {
+    if let Some(factory) = gst::ElementFactory::find(name) {
+        let caps = get_src_caps(&factory, pad);
+        Some(BashGstElement {
+            factory: factory,
+            element: gst::ElementFactory::make(name, None).unwrap(),
+            caps: caps,
+        })
+    } else {
+        None
+    }
+}
+
+pub fn init() {
+    gst::init().unwrap();
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            gst::ElementFactory::list_filter(
+                &LIST,
+                &gst::Caps::new_empty(),
+                gst::PadDirection::Sink,
+                false,
+            ),
+            Vec::<ElementFactory>::new()
+        )
+    }
+
+    #[test]
+    fn test2() {
+        gst::init().unwrap();
+
+        let caps = gst::Caps::new_any();
+        let factory = gst::ElementFactory::find("oggmux").unwrap();
+
+        assert_ne!(factory.can_sink_all_caps(&caps), true);
+    }
+
+    #[test]
+    fn test3() {
+        gst::init().unwrap();
+
+        let caps = gst::Caps::new_any();
+        let factory = gst::ElementFactory::find("filesink").unwrap();
+
+        assert!(factory.can_sink_all_caps(&caps));
+    }
+
+    #[test]
+    fn test4() {
+        gst::init().unwrap();
+        let element = find_element("udpsink", None).unwrap();
+
+        for x in element.get_property_names(&[], None) {
+            println!("-- {:?}", x);
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,344 @@
+mod gstreamer;
+
+use nom::branch::alt;
+use nom::bytes::complete::{is_a, is_not, take};
+use nom::character::complete::{char, space0, space1};
+use nom::combinator::{map_res, not, peek};
+use nom::multi::{many0, many1};
+use nom::sequence::{delimited, preceded, separated_pair, terminated, tuple};
+use nom::IResult;
+
+use shell_completion::{BashCompletionInput, CompletionInput, CompletionSet};
+
+fn parse(s: &str) -> (i8, IResult<&str, Vec<(&str, Vec<(&str, &str)>)>>) {
+    let mut index = -1;
+
+    let res = many1(separated_pair(
+        preceded(
+            map_res(
+                tuple((
+                    char('!'),
+                    space1,
+                    many0(tuple((char('-'), is_not(" \t"), space0))),
+                )),
+                |x| {
+                    index = index + 1;
+                    Ok::<_, nom::Err<&str>>(x)
+                },
+            ),
+            map_res(is_not(" \t"), |s: &str| {
+                if s.chars()
+                    .all(|c| c.is_alphanumeric() || c == '-' || c == '_')
+                {
+                    Ok(s)
+                } else {
+                    Err(nom::Err::<&str>::Error("error"))
+                }
+            }),
+        ),
+        tuple((space0, many0(tuple((char('-'), is_not(" \t"), space0))))),
+        terminated(
+            many0(separated_pair(
+                preceded(
+                    peek(not(char('!'))),
+                    map_res(is_not("= \t"), |s: &str| {
+                        if s.chars()
+                            .all(|c| c.is_alphanumeric() || c == '-' || c == '_')
+                        {
+                            Ok(s)
+                        } else {
+                            Err(nom::Err::<&str>::Error("error"))
+                        }
+                    }),
+                ),
+                tuple((space0, char('='), space0)),
+                alt((
+                    delimited(
+                        is_a("\"\'"),
+                        is_not("\"\'"),
+                        tuple((
+                            take(1u8),
+                            space1,
+                            many0(tuple((char('-'), is_not(" \t"), space0))),
+                        )),
+                    ),
+                    map_res(
+                        terminated(
+                            terminated(is_not(" \t"), space1),
+                            many0(tuple((char('-'), is_not(" \t"), space0))),
+                        ),
+                        |s: &str| {
+                            if s.contains("\'") || s.contains("\"") {
+                                Err(nom::Err::<&str>::Error("error"))
+                            } else {
+                                Ok(s)
+                            }
+                        },
+                    ),
+                )),
+            )),
+            many0(tuple((char('-'), is_not(" \t"), space0))),
+        ),
+    ))(s);
+
+    (index, res)
+}
+
+fn is_remainder_sane(input: &BashCompletionInput, rem: &str) -> bool {
+    let res: IResult<&str, &str> = preceded(char('!'), space1)(rem);
+    if let Ok(("", _)) = res {
+        true
+    } else if rem
+        .chars()
+        .all(|x| x.is_alphanumeric() || x == '-' || x == '_')
+    {
+        true
+    } else {
+        false
+    }
+}
+
+fn main() {
+    gstreamer::init();
+
+    let input = BashCompletionInput::from_env().expect("Missing expected environment variables");
+    
+    if !input
+        .current_word()
+        .chars()
+        .all(|x| x.is_alphanumeric() || x == '_' || x == '-')
+    {
+        return;
+    }
+
+    let current_word = if input.current_word().is_empty() {
+        None
+    } else {
+        Some(input.current_word())
+    };
+    
+    let args = {
+        let mut c = input.args();
+        c[0] = "!";
+        c
+    }
+    .join(" ");
+
+    if let (i, Ok((rem, parsed))) = parse(&args) {
+        if !is_remainder_sane(&input, rem) {
+            return;
+        }
+
+        let len = parsed.len();
+        assert!(len > 0);
+
+        if len as i8 == i
+            || (parsed[i as usize].1.len() == 0 && current_word == Some(parsed[i as usize].0))
+        {
+            if i == 0 {
+                return gstreamer::get_elements(current_word).suggest();
+            }
+
+            let index = (i - 1) as usize;
+
+            if let Some(element) = gstreamer::find_element(parsed[index].0, None) {
+                element.get_compatible_elements(current_word).suggest();
+            }
+        } else if let Some(element) = gstreamer::find_element(parsed[i as usize].0, None) {
+            let arr = parsed[i as usize]
+                .1
+                .iter()
+                .map(|x| x.0)
+                .collect::<Vec<&str>>();
+
+            element.get_property_names(&arr, current_word).suggest();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test0() {
+        assert_eq!(
+            parse("! "),
+            (
+                0i8,
+                Err(nom::Err::Error(nom::error::Error {
+                    input: "",
+                    code: nom::error::ErrorKind::IsNot
+                }))
+            )
+        );
+    }
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            parse("! filesrc "),
+            (1, Ok(("", vec![("filesrc", vec![])])))
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            parse("! filesrc ! fakesink "),
+            (1, Ok(("", vec![("filesrc", vec![]), ("fakesink", vec![])])))
+        );
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(
+            parse("! filesrc ! fakesink ! "),
+            (
+                2,
+                Ok(("! ", vec![("filesrc", vec![]), ("fakesink", vec![])]))
+            )
+        );
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(
+            parse("! filesrc ! fakesink !"),
+            (
+                1,
+                Ok(("!", vec![("filesrc", vec![]), ("fakesink", vec![])]))
+            )
+        );
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(
+            parse("! fakesrc ! fakesink name = test   !    "),
+            (
+                2,
+                Ok((
+                    "!    ",
+                    vec![("fakesrc", vec![]), ("fakesink", vec![("name", "test")])]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesi "),
+            (
+                2,
+                Ok((
+                    "",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesi", vec![])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test7() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesink name   "),
+            (
+                2,
+                Ok((
+                    "name   ",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesink", vec![])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test8() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesink name =  "),
+            (
+                2,
+                Ok((
+                    "name =  ",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesink", vec![])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test9() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesink name = prop = 1"),
+            (
+                2,
+                Ok((
+                    "= 1",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesink", vec![("name", "prop")])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test10() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesink name = s -e prop v = 1"),
+            (
+                2,
+                Ok((
+                    "prop v = 1",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesink", vec![("name", "s")])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test11() {
+        assert_eq!(
+            parse("! fakesrc ! identity name = test  !   fakesink name = s prop v = 1 ! abc "),
+            (
+                2,
+                Ok((
+                    "prop v = 1 ! abc ",
+                    vec![
+                        ("fakesrc", vec![]),
+                        ("identity", vec![("name", "test")]),
+                        ("fakesink", vec![("name", "s")])
+                    ]
+                ))
+            )
+        );
+    }
+
+    #[test]
+    fn test12() {
+        assert_eq!(
+            parse("! filesrc ! fakesink"),
+            (1, Ok(("", vec![("filesrc", vec![]), ("fakesink", vec![])])))
+        );
+    }
+}
